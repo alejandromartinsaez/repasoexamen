aebffea9b105b0c0a56439e7cd12c0d993f68a39
SamuelBarant <samuel.barant@gmail.com>
2025-05-28 12:18:27 +0200

    Finalizado el ejercicio de Piramide.

--- src/piramide/Piramide.java
+++ src/piramide/Piramide.java
@@ -1,4 +1,21 @@
 package piramide;
 
 public class Piramide {
+    public static void main(String[] args) {
+        int base = 10;
+
+        for (int i = 0; i < base-1; i++) {
+            System.out.print(" ");
+        }
+        System.out.print("0");
+        for (int i = 0; i < base; i++) {
+            for (int j = 0; j < base-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < (2*i-1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
